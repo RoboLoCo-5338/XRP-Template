83b092845959825a72f0c5a139d927c2ac47f05c
KAVIN MURALIKRISHNAN (921563) <921563@lcps.org>
2024-09-04 13:11:18 -0400

    fix task 7

--- src/main/java/frc/robot/commands/DriveCommands.java
+++ src/main/java/frc/robot/commands/DriveCommands.java
@@ -215,6 +215,6 @@ public class DriveCommands {
   //TODO: Task 6-Write a Command(function or class) that causes the XRP to drive until the distance returned by the rangefinder
   //is less than 2 inches.
 
-  //TODO: Task 7
+  //TODO: Task 7-Write a Sequential Command Group to move the XRP backwards 2 inches, set the arm preset to index 1, spin the XRP 360 degrees
   //and move the XRP forward 3 inches.
 }
