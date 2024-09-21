6469e539efd3645040d4f19201e3739cf4cdfe8d
advay17 <921563@lcps.org>
2024-09-21 17:16:18 -0400

    commented stuff

--- src/main/java/frc/robot/subsystems/XRPDrivetrain.java
+++ src/main/java/frc/robot/subsystems/XRPDrivetrain.java
@@ -73,4 +73,13 @@ public class XRPDrivetrain extends SubsystemBase {
   public void simulationPeriodic() {
     // This method will be called once per scheduler run during simulation
   }
+  // @Override
+  // public void periodic() {
+  //   // This method will be called once per scheduler run
+  // }
+
+  // @Override
+  // public void simulationPeriodic() {
+  //   // This method will be called once per scheduler run during simulation
+  // }
 }
