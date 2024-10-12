909cf4c5b7d070d43350353163edc2a8de581f68
RyanW <ryan.wang.2022.me@gmail.com>
2024-10-11 22:18:09 -0400

    Task 5 Completion

--- src/main/java/frc/robot/subsystems/Servo.java
+++ src/main/java/frc/robot/subsystems/Servo.java
@@ -14,4 +14,13 @@ public class Servo extends SubsystemBase {
     //TODO: Task 5-Write functions to control the arm. You should be able to set the arm's angle and get the angle
     //it is at.
     //HINT: Type 'm_armServo.' (without quotes) in a method body to see all of the different methods the servo has.
+    public void setAngle(double angle) {
+        //sets angle to this
+        m_armServo.setAngle(angle);
+    }
+
+    public void adjustAngleBy(double adjustAmount) {
+        //makes the current angle larger or smaller by adjusting angle by adjustAmount
+        m_armServo.setAngle(adjustAmount+m_armServo.getAngle());
+    }
 }
