e4e63f383069bce058695dbd7b709c878b86dc88
Saanvi Korem <saanvikorem@gmail.com>
2024-10-23 23:49:06 -0400

    Task 6 Completion

--- src/main/java/frc/robot/commands/ServoCommands.java
+++ src/main/java/frc/robot/commands/ServoCommands.java
@@ -4,5 +4,24 @@ package frc.robot.commands;
 public class ServoCommands {
     //TODO: Task 6-Write commands to move the arm up, down, and to a specific preset. The latter should be done by taking in an integer input.
     //The presets are in the constants file.
-    
+    public static InstantCommand moveArmUp(integer numberr){
+        return new InstantCommand()->{
+            double angleNew = servo.getArmAngle() + numberr;
+            servo.setArmAngle(angleNew);
+        }
+    }
+    public static InstantCommand moveArmDown(integer numberr){
+        return new InstantCommand () -> {
+            double angleNew = servo.getArmAngle() - numberr;
+            servo.setArmAngle(angleNew);
+        }
+    }
+    public static InstantCommand presetArm(int presetI){
+        return new InstantCommand () -> {
+            if(presetI < Constants.servoPresets.length && presetI >=0){
+                double AngleA = Constants.servoPresets[presetI];
+                servo.setArmAngle(AngleA);
+            }
+        }
+    }
 }
